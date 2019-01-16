9d95d8f02e124402e02ff348c58442066b97c80f
yuvaldolev <yuvaldolev2@gmail.com>
2019-01-16 17:31:13 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,60 @@
+project "GoogleTest"
+    kind "StaticLib"
+    language "C++"
 
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "googletest/include/gtest/gtest.h"
+        "googletest/src/gtest.cc",
+        "googletest/src/gtest-death-test.cc",
+        "googletest/src/gtest-filepath.cc",
+        "googletest/src/gtest-matchers.cc",
+        "googletest/src/gtest-port.cc",
+        "googletest/src/gtest-printers.cc",
+        "googletest/src/gtest-test-part.cc",
+        "googletest/src/gtest-typed-test.cc"
+    }
+
+    includedirs
+    {
+        "googletest"
+        "googletest/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
+project "GoogleTestMain"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "googletest/include/gtest/gtest.h"
+        "googletest/src/src/gtest_main.cc",
+    }
+
+    includedirs
+    {
+        "googletest"
+        "googletest/include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
